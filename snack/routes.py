56b9d2032f22ba085cca368d712d1c523962131f
jod35 <jodestrevin@gmail.com>
2019-09-16 12:29:57 -0400

    first commit

--- snack/routes.py
+++ snack/routes.py
@@ -4,3 +4,7 @@ from flask import render_template,redirect,request,url_for
 @app.route('/')
 def index():
     return  render_template('index.html')
+
+@app.route('/signup')
+def sign_up():
+    return render_template('sign.html')
