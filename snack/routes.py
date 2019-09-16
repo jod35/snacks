4dae375e71286c1b8a96330e486fd1064b2ab122
jod35 <jodestrevin@gmail.com>
2019-09-16 15:24:23 -0400

    created the logins

--- snack/routes.py
+++ snack/routes.py
@@ -2,7 +2,7 @@ from snack import app,db
 from flask import render_template,redirect,request,url_for,flash
 from snack.models import User
 from flask_bcrypt import Bcrypt
-
+from flask_login import login_user,logout_user,current_user
 
 bcrypt=Bcrypt(app)
 
@@ -30,3 +30,26 @@ def sign_up():
         flash('Account for %s created successfully'%name)
         return redirect('/signup')
     return render_template('sign.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def Login():
+    email=request.form.get('email')
+    password=request.form.get('password')
+
+    user=User.query.filter_by(email=email).first()
+
+    if not user:
+        flash('Invalid Username or Password')
+    if user and bcrypt.check_password_hash(user.password,password):
+        login_user(user)
+        return redirect('/')
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect('/')
+
+@app.route('/addsnacks')
+def current_snacks():
+    return render_template('snacks.html')
